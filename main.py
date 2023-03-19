1dac13e3b71347852696f3bb0af93a2766bb2ac6
David Grau <fe80grau@gmail.com>
2023-03-19 12:40:47 +0100

    -

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, make_response
+import urllib.parse
+import urllib.request
+
+app = Flask(__name__)
+
+@app.route('/filter_m3u')
+def filter_m3u():
+    url = request.args.get('url')
+    text = request.args.get('text')
+
+    if url is None:
+        return 'Debe proporcionar una URL m3u'
+    elif text is None:
+        return 'Debe proporcionar un texto de búsqueda'
+
+
+    # Leer el archivo m3u y filtrar los elementos según los parámetros en EXTINF
+    filtered_m3u = '#EXTM3U\n'
+    with urllib.request.urlopen(urllib.parse.unquote(url)) as response:
+        data = response.read().decode('utf-8')
+        lines = data.split('\n')
+        for i, line in enumerate(lines):
+            if '#EXTINF' in line and urllib.parse.unquote(text) in line:
+                # Si la línea contiene parámetros en EXTINF y el texto buscado, agregarla al archivo filtrado
+                filtered_m3u += line + '\n'
+                if i + 1 < len(lines):
+                    # Si hay una siguiente línea, que se asume que es la URL, agregarla también al archivo filtrado
+                    filtered_m3u += lines[i+1] + '\n'
+
+    # Crear la respuesta y agregar el encabezado
+    response = make_response(filtered_m3u)
+    response.headers['Content-Type'] = 'application/vnd.apple.mpegurl'
+
+    # Devolver la respuesta
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=False, port=5001)
